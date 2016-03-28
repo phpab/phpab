52f64a3e5d2cb544ff687801f45496c043bd9d05
Walter Tamboer <walter@tamboer.nl>
2016-03-28 13:08:24 +0200

    Added a docblock (also to test this commit - it should bind to issue #4)

--- example/example.php
+++ example/example.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of phpab/phpab (https://github.com/phpab/phpab)
+ *
+ * @link https://github.com/phpab/phpab for the canonical source repository
+ * @copyright Copyright (c) 2015-2016 phpab. (https://github.com/phpab)
+ * @license https://raw.githubusercontent.com/phpab/phpab/master/LICENSE MIT
+ */
+
 require '../vendor/autoload.php';
 
 use PhpAb\AbRunner;
