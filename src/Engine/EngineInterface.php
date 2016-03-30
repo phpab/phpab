20d21e51b93d8484b033d7007ba32cd7dd53a132
Patrick Heller <paddy.heller@gmail.com>
2016-03-30 21:33:38 +0200

    initial PoC

--- src/Engine/EngineInterface.php
+++ src/Engine/EngineInterface.php
@@ -5,8 +5,10 @@ namespace Phpab\Phpab\Engine;
 use Phpab\Phpab\Analytics\AnalyticsInterface;
 use Phpab\Phpab\Exception\TestCollisionException;
 use Phpab\Phpab\Exception\TestNotFoundException;
+use Phpab\Phpab\Participation\FilterInterface;
 use Phpab\Phpab\Participation\StorageInterface;
 use Phpab\Phpab\Test\TestInterface;
+use Phpab\Phpab\Variant\ChooserInterface;
 
 interface EngineInterface
 {
@@ -49,11 +51,21 @@ interface EngineInterface
     /**
      * Adds a test to the Engine
      *
-     * @param \Phpab\Phpab\Test\TestInterface $test
-     * 
+     * @param \Phpab\Phpab\Test\TestInterface                 $test
+     * @param array                                           $options
+     * @param \Phpab\Phpab\Participation\FilterInterface|null $participationFilter
+     * @param \Phpab\Phpab\Variant\ChooserInterface|null      $variantChooser
+     *
      * @throws TestCollisionException
+     *
+     * @return null
      */
-    public function addTest(TestInterface $test);
+    public function addTest(
+        TestInterface $test,
+        $options = [],
+        FilterInterface $participationFilter = null,
+        ChooserInterface $variantChooser = null
+    );
 
     /**
      * Starts the tests
