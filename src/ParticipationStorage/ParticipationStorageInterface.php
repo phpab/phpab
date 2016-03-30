2fddac63704aaca65daeb759f48bc2c5eea4d842
Patrick Heller <paddy.heller@gmail.com>
2016-03-30 19:28:30 +0200

    clearify comments

--- src/ParticipationStorage/ParticipationStorageInterface.php
+++ src/ParticipationStorage/ParticipationStorageInterface.php
@@ -17,38 +17,38 @@ interface ParticipationStorageInterface
     /**
      * Checks if the test has a participation set.
      *
-     * @param string $identifier The attribute name
+     * @param string $identifier The tests identifier
      *
-     * @return bool true if the attribute is defined, false otherwise
+     * @return bool true if the test participation is defined, false otherwise
      */
     public function has($identifier);
 
     /**
      * Returns the participation value (Variant or false).
      *
-     * @param string $identifier The attribute name
+     * @param string $identifier The tests identifier name
      *
      * @return mixed
      */
     public function get($identifier);
 
     /**
-     * Sets an attribute.
+     * Sets participation value for a test
      *
-     * @param string $identifier
+     * @param string $identifier The tests identifier
      * @param mixed  $participation The participated variant
      */
     public function set($identifier, $participation);
 
     /**
-     * Returns attributes.
+     * Returns all stored tests.
      *
      * @return array Attributes
      */
     public function all();
 
     /**
-     * Removes an attribute.
+     * Removes a stored test.
      *
      * @param string $identifier
      *
