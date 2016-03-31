a5122ba0fadfd401670e1f57739a7e7169fac892
Patrick Heller <paddy.heller@gmail.com>
2016-03-31 22:02:59 +0200

    implement chooser

---
+++ src/Variant/RandomChooser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PhpAb\Variant;
+
+class RandomChooser implements ChooserInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function chooseVariant($variants)
+    {
+        $chosen = array_rand($variants);
+
+        return isset($variants[$chosen]) ? $variants[$chosen] : null;
+    }
+
+
+}
---
+++ tests/Variant/RandomChooserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PhpAb\Variant;
+
+/**
+ * Override time() in current namespace for testing
+ *
+ * @return int
+ */
+function array_rand()
+{
+    return 1;
+}
+
+class RandomChooserTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testChooseVariants()
+    {
+        // Arrange
+        $variant1 = $this->getMock(VariantInterface::class, [], ['v1']);
+        $variant2 = $this->getMock(VariantInterface::class, [], ['v2']);
+        $variant3 = $this->getMock(VariantInterface::class, [], ['v3']);
+
+        $chooser = new RandomChooser();
+
+        // Act
+        $chosen = $chooser->chooseVariant([
+            $variant1,
+            $variant2,
+            $variant3,
+        ]);
+
+        // Assert
+        $this->assertSame($variant2, $chosen);
+    }
+
+    public function testChooseVariantsFromEmpty()
+    {
+        // Arrange
+        $chooser = new RandomChooser();
+
+        // Act
+        $chosen = $chooser->chooseVariant([]);
+
+        // Assert
+        $this->assertEquals(null, $chosen);
+    }
+    
+}
