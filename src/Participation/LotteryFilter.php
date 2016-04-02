a5bdb1ab446f045139ed6bf4b3ff803570d38d6b
Patrick Heller <paddy.heller@gmail.com>
2016-04-02 15:21:02 +0200

    fix walters comments

--- src/Participation/LotteryFilter.php
+++ src/Participation/LotteryFilter.php
@@ -5,7 +5,7 @@ namespace PhpAb\Participation;
 class LotteryFilter implements FilterInterface
 {
     /**
-     * @float
+     * @var int
      */
     private $propability;
 
@@ -18,6 +18,16 @@ class LotteryFilter implements FilterInterface
      */
     public function __construct($propability)
     {
+        // ensure that we have a float since we cannot typehint
+        // it in the constructor for PHP versions < 7
+        if (! is_int($propability)) {
+            throw new \InvalidArgumentException('The propability must be of type int.'.gettype($propability).' given');
+        }
+
+        if ($propability < 0 || $propability > 100) {
+            throw new \InvalidArgumentException('the probability must be 0 <=> 100');
+        }
+
         $this->propability = $propability;
     }
 
@@ -29,14 +39,8 @@ class LotteryFilter implements FilterInterface
     {
         $propability = $this->propability;
 
-        // ensure that we have a float since we cannot typehint
-        // it in the constructor for PHP versions < 7
-        if (! is_int($propability)) {
-            throw new \InvalidArgumentException('The propability must be of type int.'.gettype($propability).' given');
-        }
-
-        if ($propability < 0 || $propability > 100) {
-            throw new \InvalidArgumentException('the probability must be 0 <=> 100');
+        if (100 === $propability) {
+            return true;
         }
 
         if (0 === $propability) {
@@ -45,10 +49,6 @@ class LotteryFilter implements FilterInterface
             return false;
         }
 
-        if (100 === $propability) {
-            return true;
-        }
-
-        return  0 === mt_rand(0, $propability);
+        return mt_rand(0, 100) <= $propability;
     }
 }
