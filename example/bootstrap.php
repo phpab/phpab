67c0bc5e5ef4bc1f305e9a1bb02b15cfae190b36
Patrick Heller <paddy.heller@gmail.com>
2016-04-02 17:55:30 +0200

    Updated Example

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -1,28 +1,24 @@
 <?php
 
-// this is pseude code because it's not implemented yet and we do not have tests
-
-$app = new myShinyWhateverFramework();
+// Engine and Analytics have to be implemented before this works
 
 $engine = new \PhpAb\Engine(
-    new \PhpAb\Storage\CookieStorage(),
-    new \PhpAb\Analytics\GoogleExperiments('UA-asfsafsaf')
+    new \PhpAb\Storage\Session()
 );
 
 $test = new \PhpAb\Test(
     'foo_test',
-    new \PhpAb\ParticipationStrategy\LotteryParticipationStrategy(0.1),
-    new \PhpAb\VariantChooser\RandomVariantChooser()
+    new \PhpAb\Participation\PercentageFilter(10),
+    new \PhpAb\Variant\RandomChooser()
 );
-$test->addVariant(new \PhpAb\DifferentThemeVariant('different_checkout_steps', $app->getEventManager()));
+
+$test->addVariant(new \PhpAb\Variant\SimpleVariant('_control'));
+$test->addVariant(new \PhpAb\Variant\SimpleVariant('variantA'));
+$test->addVariant(new \PhpAb\Variant\SimpleVariant('variantB'));
+$test->addVariant(new \PhpAb\Variant\SimpleVariant('variantC'));
 
 // Add some tests
 $engine->addTest($test);
 
 // Start testing. Must occur before the EventCycle of the app starts
 $engine->start();
-
-// Start the app
-// The Events of the app fire
-// e.g. EVENT_MERGE_CONFIG where our Variant listens to
-$app->run();
