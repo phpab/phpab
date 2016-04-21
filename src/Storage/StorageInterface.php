7e4ac2b17e74279e8fb1fcebe6ea2925a2f98139
Walter Tamboer <walter@tamboer.nl>
2016-04-21 21:00:03 +0200

    Updated the year in the copyright.

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -3,7 +3,7 @@
  * This file is part of phpab/phpab. (https://github.com/phpab/phpab)
  *
  * @link https://github.com/phpab/phpab for the canonical source repository
- * @copyright Copyright (c) 2005-2016 phpab. (https://github.com/phpab/)
+ * @copyright Copyright (c) 2015-2016 phpab. (https://github.com/phpab/)
  * @license https://raw.githubusercontent.com/phpab/phpab/master/LICENSE.md MIT
  */
 
