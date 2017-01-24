f0c7b14886ecfd2ce86d7f2814f978f88d018673
Patrick Heller <paddy.heller@gmail.com>
2017-01-24 21:18:59 +0100

    update example

--- example/01-bootstrap.php
+++ example/01-bootstrap.php
@@ -8,28 +8,15 @@
  */
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use PhpAb\Storage\Adapter\Cookie;
 use PhpAb\Storage\Storage;
-use PhpAb\Participation\Manager;
-use PhpAb\Analytics\DataCollector\Google;
 use PhpAb\Filter\Percentage;
 use PhpAb\Chooser\RandomChooser;
 use PhpAb\Engine\Engine;
 use PhpAb\Test\Test;
 use PhpAb\Variant\SimpleVariant;
-use PhpAb\Analytics\Renderer\Google\GoogleUniversalAnalytics;
 
-// Create a Storage and its Adapter
-$adapter = new Cookie('phpab');
-$storage = new Storage($adapter);
-
-// Create a Participation Manager
-$manager = new Manager($storage);
-
-$dispatcher->addSubscriber($analyticsData);
-
-// Create a Data Collector
-$analyticsData = new Google();
+// Create the tested subject
+$user = new \PhpAb\Subject(new \PhpAb\Storage\CookieStorage('phpab'));
 
 // Create the Engine
 $engine = new Engine();
@@ -44,17 +31,5 @@ $test->addVariant(new SimpleVariant('_variant2'));
 // Add the tests to the Engine
 $engine->addTest($test, new Percentage(50), new RandomChooser());
 
-// Pseudo: if($user->isAdmin)
-// If the user is admin, he should not participate at the test
-// $manager->participate('foo_test', null);
-// Pseudo: if($app->inDevelopment() and $_GET['phpab']['foo_test])
-// $manager->participate('foo_test', $_GET['phpab']['foo_test]);
-// Start testing. Must occur before the EventCycle of the app starts
 // Start the engine
-$engine->test($manager);
-
-// Create the Analytics object and pass the Data Collector data to it
-$analytics = new GoogleUniversalAnalytics($analyticsData->getTestsData());
-
-// Execute the Analytics functionality
-var_dump($analytics->getScript());
+$engine->test($user);
